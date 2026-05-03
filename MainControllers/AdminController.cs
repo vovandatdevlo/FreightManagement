c29b0fba86ac187c2163f582d8d8807df7b78406
Vo Van Dat <vovandat280806@gmail.com>
2026-05-04 00:20:54 +0700

    add chuc nang

--- MainControllers/AdminController.cs
+++ MainControllers/AdminController.cs
@@ -1,6 +1,7 @@
 ﻿using FreightManagement.Service;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using FreightManagement.DTOs;
 
 namespace FreightManagement.MainControllers
 {
